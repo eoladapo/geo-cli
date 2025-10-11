6aecf91c815ee4c8f77f039f87ae3f50f4f38bf0
oladapo <oladapoelijah49@gmail.com>
2025-10-11 16:53:43 +0100

    setup + custom plugin for bundling

---
+++ src/index.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import * as esbuild from 'esbuild-wasm';
+import { write } from 'fs';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el!);
+
+const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  async function onClick() {
+    if (!ref.current) {
+      return;
+    }
+
+    // for transform or transpile only
+    // const result = await ref.current.transform(input, {
+    //   loader: 'jsx',
+    //   target: 'es2015',
+    // });
+
+    // for transform + bundle
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: { 'process.env.NODE_ENV': '"production"', global: 'window' },
+    });
+
+    console.log(result.outputFiles[0].text);
+
+    setCode(result.code);
+  }
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+root.render(<App />);
