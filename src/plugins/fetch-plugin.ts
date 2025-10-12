187381d353fc2e3077750c07691a9aa2165d8ed0
oladapo <oladapoelijah49@gmail.com>
2025-10-12 15:59:19 +0100

    made a lot of code refactoring and added more onLoad plugin functionality

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,16 +10,15 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
+      // handle root entry file of 'index.js'
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
+
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        // console.log('onLoad', args);
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-
         // check to see if we have already fetched this file and it is in the cache
         //if it is in the cache, return it immediately
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
@@ -27,7 +26,30 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult as esbuild.OnLoadResult;
         }
+      });
 
+      // handle CSS files
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const escaped = data.replace(/\n/g, '').replace(/"/g, '\\"').replace(/'/g, "\\'");
+        const contents = `const style = document.createElement('style'); 
+            style.innerText = ${JSON.stringify(escaped)}; 
+              document.head.appendChild(style)`;
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      // handle all other files
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
