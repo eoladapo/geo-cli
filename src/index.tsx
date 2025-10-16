0957e34d580a4bf0c5868546f067d74dd97b81c0
oladapo <oladapoelijah49@gmail.com>
2025-10-16 12:47:57 +0100

    configuring the code editor to achieve the purpose of the cli

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/Code-Editor';
 
 const el = document.getElementById('root');
 
@@ -75,6 +76,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor initialValue="const a = 1;" onChange={(value) => setInput(value)} />
       <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
