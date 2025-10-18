d0ecba7ded2647a38545aa5051c98e2238011210
oladapo <oladapoelijah49@gmail.com>
2025-10-18 15:50:24 +0100

    impl the resize for the editor

--- src/components/Resizeable.tsx
+++ src/components/Resizeable.tsx
@@ -9,7 +9,13 @@ interface ResizableProps {
 
 const ReSizeable: React.FC<ResizableProps> = ({ direction, children }) => {
   return (
-    <ResizableBox height={300} width={Infinity} resizeHandles={['s']}>
+    <ResizableBox
+      height={300}
+      width={Infinity}
+      maxConstraints={[Infinity, window.innerHeight * 0.9]}
+      minConstraints={[Infinity, 24]}
+      resizeHandles={['s']}
+    >
       {children}
     </ResizableBox>
   );
