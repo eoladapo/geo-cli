16b8b0cebde72119b7155364b24c41c8c29739fa
oladapo <oladapoelijah49@gmail.com>
2025-10-25 19:21:29 +0100

    updating the CodeCell and TextEditor

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -1,17 +1,23 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CodeEditor from './Code-Editor';
 import Preview from './Preview';
 import bundle from '../bundler/index';
 import ReSizeable from './Resizeable';
+import { Cell } from '../redux/cell';
+import { useActions } from '../hooks/use-actions';
 
-const CodeCell = () => {
+interface CodeCellProps {
+  cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
   const [err, setErr] = useState('');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(input);
+      const output = await bundle(cell.content);
       setCode(output.code);
       setErr(output.err);
     }, 1000);
@@ -19,17 +25,14 @@ const CodeCell = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <ReSizeable direction="vertical">
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <ReSizeable direction="horizontal">
-          <CodeEditor initialValue="const a = 1;" onChange={(value) => setInput(value)} />
+          <CodeEditor initialValue={cell.content} onChange={(value) => updateCell(cell.id, value)} />
         </ReSizeable>
-        {/* <div>
-          <button onClick={onClick}>Submit</button>
-        </div> */}
         <Preview code={code} err={err} />
       </div>
     </ReSizeable>
