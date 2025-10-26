60ed9f636c5fba398986a17b19d8cbfdd4fdc913
oladapo <oladapoelijah49@gmail.com>
2025-10-26 18:49:59 +0100

    add-cell fixes

--- src/components/CellList.tsx
+++ src/components/CellList.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './CellListItem';
+import AddCell from './AddCell';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => {
     return order.map((id) => data[id]);
   });
 
-  const renderedCells = cells.map((cell) => <CellListItem key={cell.id} cell={cell} />);
-  return <div>CellList</div>;
+  const renderedCells = cells.map((cell) => (
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem key={cell.id} cell={cell} />
+    </Fragment>
+  ));
+  return (
+    <div>
+      {renderedCells}
+      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
